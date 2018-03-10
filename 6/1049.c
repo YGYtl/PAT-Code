7b9762670ec8c7a071000223dd46112801a0c944
Ygytl <ygy.cs06@outlook.com>
2018-03-10 22:16:36 +0800

    1049.c

---
+++ 6/1049.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+  int n;
+  scanf("%d", &n);
+  int i;
+  double a[100001] = {0};
+  double sum=0.0;
+  for(i=1; i<=n; i++)
+  {
+    scanf("%lf", &a[i]);
+    sum = sum + a[i]*i*(n-i+1);
+  }
+  printf("%.2f", sum);
+  return 0;
+}
