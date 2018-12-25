8809fa77ed901bd60e428dc147caf4fc7d9e7870
Ygytl <ygy.cs06@outlook.com>
2018-12-25 16:39:53 +0800

    1089.cpp

--- 10/1089.cpp
+++ 10/1089.cpp
@@ -1,24 +1,56 @@
-#include <iostream>
-#include <vector>
-#include <cmath>
+#include<iostream>
+#include<algorithm>
 using namespace std;
-int main() {
-    int n;
-    cin >> n;
-    vector<int> v(n+1);
-    for (int i = 1; i <= n; i++) cin >> v[i];
-    for (int i = 1; i <= n; i++) {
-        for (int j = i + 1; j <= n; j++) {
-            vector<int> lie, a(n + 1, 1);
-            a[i] = a[j] = -1;
-            for (int k = 1; k <= n; k++)
-                if (v[k] * a[abs(v[k])] < 0) lie.push_back(k);
-            if (lie.size() == 2 && a[lie[0]] + a[lie[1]] == 0) {
-                cout << i << " " << j;
-                return 0;
-            }
-        }
-    }
-    cout << "No Solution";
-    return 0;
+
+int cmp(int a, int b)
+{
+	return a < b;
+}
+
+int main()
+{
+	int n;
+	cin >> n;
+	int i, k=1, temp=1;
+	int *a = new int [n];
+	int *b = new int [n];
+	for (i=0; i<n; i++) cin >> a[i];
+	for (i=0; i<n; i++) cin >> b[i];
+	int j;
+	for (i=0; i<n-1&&b[i]<=b[i+1]; i++);
+	for (j=i+1; j<n&&a[j]==b[j]; j++);
+	
+	if(j==n)
+	{
+		cout << "Insertion Sort" << endl;
+		sort(a, a+i+2, cmp);
+	}
+	else{
+		cout << "Merge Sort" << endl;
+		while(temp)
+		{
+			temp = 0;
+			
+			for(i=0; i<n; i++)
+			{
+				if(a[i]!=b[i])
+				{
+					temp = 1;
+					break;
+				}
+			}
+			k = k * 2;
+			for(i=0; i<n/k; i++)
+			{
+				sort(a+i*k, a+(i+1)*k, cmp);
+			}
+			sort(a+n/k*k, a+n, cmp);
+		}
+	}
+	for (i=0; i<n-1; i++)
+	{
+		cout << a[i] << " ";
+	}
+	cout << a[i] << endl;
+	return 0;
 }
