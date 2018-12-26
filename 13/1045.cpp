d9bef6601af3536055f46e5dd77dd45078b18715
Ygytl <ygy.cs06@outlook.com>
2018-12-26 09:16:14 +0800

    1045.cpp

---
+++ 13/1045.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<cstdio>
+using namespace std;
+int main()
+{
+	int n, m;
+	int a[201], b[10001];
+	scanf("%d %d", &n, &m);
+	for(int i=0; i<m; i++) scanf("%d", &a[i]);
+	int l; scanf("%d", &l);
+	for(int i=0; i<l; i++) scanf("%d", &b[i]);
+	
+	return 0;
+}
